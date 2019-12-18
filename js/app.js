1ff3ae03271e0385df5d472aee835a175dc5fe47
Cale <cale@midnightcheese.com>
2019-12-17 20:55:47 -0600

    Import project.

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var t;
+
+$(document).foundation();
+
+$(document).ready(function() {
+    imageTimer();
+});
+
+imageTimer = function() {
+    clearTimeout(t);
+    timer(60000,reloadImage);
+}
+
+reloadImage = function() {
+    var randomId = new Date().getTime();
+    $(".current-view-image img").attr('src', 'http://nashville.calemooth.com/nashville.jpg?r=' + randomId);
+    imageTimer();
+}
+
+timer = function(time,func) {
+    t = window.setTimeout(function() {
+        func();
+    }, time)
+}
